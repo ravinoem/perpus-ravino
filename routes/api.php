d4e61f2b8ed5435e305960f50079c1414ae4ab36
Ravino Elang Mahardika <ravinomahardika@gmail.com>
2023-03-20 23:21:10 +0700

    Changed API Routing

--- routes/api.php
+++ routes/api.php
@@ -1,5 +1,6 @@
 <?php
 
+use App\Http\Controllers\BookController;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
 
@@ -18,8 +19,8 @@ Route::middleware('auth:api')->get('/user', function (Request $request) {
     return $request->user();
 });
 
-Route::get('/book', 'BookController@index');
-Route::post('/book', 'BookController@create');
-Route::get('/book/{id}', 'BookController@detail');
-Route::put('/book/{id}', 'BookController@update');
-Route::delete('/book/{id}', 'BookController@delete');
+Route::get('/book', [BookController::class, 'index']);
+Route::post('/book', [BookController::class, 'create']);
+Route::get('/book/{id}', [BookController::class, 'detail']);
+Route::put('/book/{id}', [BookController::class, 'update']);
+Route::delete('/book/{id}', [BookController::class, 'delete']);
